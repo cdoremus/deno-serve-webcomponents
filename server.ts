e526e80d531187b3b0361629ac53178fdde46878
Craig Doremus <cdoremus@gmail.com>
2024-01-15 18:59:48 -0500

    Cleaned up server,ts

--- server.ts
+++ server.ts
@@ -3,30 +3,36 @@ import {
   serveFile,
 } from "https://deno.land/std@0.207.0/http/file_server.ts";
 
-const handler = (req: Request) => {
+const handler = async (req: Request) => {
   const pathname = new URL(req.url).pathname;
   console.log("Pathname", pathname);
-
-  if (pathname === "/") {
-    return serveFile(req, "./static/index.html");
-  }
-
-  if (pathname.startsWith("/")) {
-    if (pathname === "/") {
-      return serveFile(req, "./static/index.html");
-    } else if (pathname.includes(".js") || pathname.includes(".css")) {
-      return serveDir(req, {
-        fsRoot: "static",
-        urlRoot: "",
-      });
+  try {
+    // GET method
+    if (req.method.toLowerCase() === "get") {
+      if (pathname === "/") {
+        return serveFile(req, "./static/index.html");
+      } else if (pathname === "/404.html") {
+        return serveFile(req, "./static/404.html");
+      } else if (pathname.includes(".js") || pathname.includes(".css")) {
+        return serveDir(req, {
+          fsRoot: "static",
+          urlRoot: "",
+        });
+      } else {
+        return serveFile(req, `./static/404.html`);
+      }
     }
+  } catch (e) {
+    console.error(`ERROR: ${e.message}`, e);
+    return await Promise.resolve(
+      new Response(
+        "Internal server error: " + e.message,
+        { status: 500 },
+      ),
+    );
   }
 
   return serveFile(req, `./static/404.html`);
-
-  // return new Response("404: File Not Found", {
-  //   status: 404,
-  // });
 };
 
 Deno.serve({ port: 8080 }, handler);
